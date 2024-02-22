fd28ff1b22a8f16ca3e4c9d752446e7e975261ff
amiranda4k <159966686+amiranda4k@users.noreply.github.com>
2024-02-21 19:52:28 -0700

    Update cis129_lab02_shortStory.py
    Added a story about Cat on an adventure

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,8 @@
-print('Hello, World!') 
+print("Under the moonlit sky, a curious cat wandered into an enchanted garden.")
+print("It discovered a talking flower, whispering secrets.")
+print("Intrigued, the cat listened all night, learning tales of hidden treasures.")
+print("As dawn broke, the cat set off on an adventure, guided by the stories of the flower.")
+# Author: Adrian Miranda 
+# My first Python lab in my CIS129 course 
+# Prints a simple short story 
+
